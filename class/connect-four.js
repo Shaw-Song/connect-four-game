cce9d22504ad0bfcac0b749cd2da7d7120a8455e
Shaw <shawsong2022@gmail.com>
2023-02-10 00:31:36 -0800

    checkwin func & change keys to up down left right

--- class/connect-four.js
+++ class/connect-four.js
@@ -21,10 +21,10 @@ class ConnectFour {
     Screen.setGridlines(true);
 
     // Replace this with real commands
-    Screen.addCommand('w', 'move cursor up', this.cursor.up);
-    Screen.addCommand('s', 'move cursor down', this.cursor.down);
-    Screen.addCommand('a', 'move cursor left', this.cursor.left);
-    Screen.addCommand('d', 'move cursor right', this.cursor.right);
+    Screen.addCommand('up', 'move cursor up', this.cursor.up);
+    Screen.addCommand('down', 'move cursor down', this.cursor.down);
+    Screen.addCommand('left', 'move cursor left', this.cursor.left);
+    Screen.addCommand('right', 'move cursor right', this.cursor.right);
     Screen.addCommand('x', 'place a X', this.placeAMoveX);
     Screen.addCommand('o', 'place a O', this.placeAMoveO);
 
@@ -87,7 +87,13 @@ class ConnectFour {
     
   }
 
-
+  __isFour(i, j, grid) {
+    if (grid[i][j] === grid[i][j + 1] && grid[i][j + 1] === grid[i][j + 2] && grid[i][j + 2] === grid[i][j + 3] && grid[i][j] !== " ") {
+      return grid[i][j];
+    } else {
+      return false;
+    }
+  }
   
   static checkWin(grid) {
 
@@ -95,8 +101,59 @@ class ConnectFour {
     // Return 'O' if player O wins
     // Return 'T' if the game is a tie
     // Return false if the game has not ended
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[0].length - 3;  j++) {
+        if (grid[i][j] === grid[i][j + 1] && grid[i][j + 1] === grid[i][j + 2] && grid[i][j + 2] === grid[i][j + 3] && grid[i][j] !== " ") {
+          return grid[i][j];
+        }
+      }
+    }
+
+    for (let j = 0; j < grid[0].length;  j++) {
+      for (let i = 0; i < grid.length - 3; i++) {
+        if (grid[i][j] === grid[i + 1][j] && grid[i + 1][j] === grid[i + 2][j] && grid[i + 2][j]=== grid[i + 3][j] && grid[i][j] !== " ") {
+          return grid[i][j];
+        }
+      }
+    }
+
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[0].length; j++) {
+        if (i + 3 < grid.length 
+          && j + 3 <  grid[0].length 
+          && grid[i][j] === grid[i + 1][j + 1] 
+          && grid[i + 1][j + 1] === grid[i + 2][j + 2] 
+          && grid[i + 2][j + 2]=== grid[i + 3][j + 3] 
+          && grid[i][j] !== " ") {
+            return grid[i][j];
+          }
+        if (i - 3 >= 0
+          && j + 3 <  grid[0].length 
+          && grid[i][j] === grid[i - 1][j + 1] 
+          && grid[i - 1][j + 1] === grid[i - 2][j + 2] 
+          && grid[i - 2][j + 2]=== grid[i - 3][j + 3] 
+          && grid[i][j] !== " ") {
+            return grid[i][j];
+          }
+        
+      }
+      
+    }
+
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] === " ") {
+          return false;
+        }
+      }
+    }
+
+
+    return "T";
+
 
   }
+
 
   static endGame(winner) {
     if (winner === 'O' || winner === 'X') {
